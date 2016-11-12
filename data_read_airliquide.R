647d6661b34d8e0992c7cebba56e7e6ea5fe210b
office206 <hp@lotustech.io>
2016-11-12 13:55:15 -0500

    adding project desc.

--- data_read_airliquide.R
+++ data_read_airliquide.R
@@ -8,3 +8,13 @@ library(dplyr)
 library(tidyr)
 library(lubricate)
 library(ggplot2)
+
+
+#Currently, small online businesses with little user activity on their search engines have no good way of evaluating the performance
+#of their search algorithms,making it difficult for them to provide an exceptional customer experience.
+
+#The goal of this engine is to create a model for airgas that can be used to measure the relevance of search results.
+#In doing so, we'll be helping enable naive customers to better sort out their seearch results for small business owners
+#to match the experience provided by more resource rich competitors like Amazon.
+#This engine will also provide airgas a model to test against. Given the queries and resulting product descriptions
+#from their Google Ad campaign. This product will help airgas to evaluate the accuracy of their search algorithms.
