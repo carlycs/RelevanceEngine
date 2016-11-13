09cab2eaaf5c086e5c514ce367c7b3f64da7bc73
office206 <hp@lotustech.io>
2016-11-12 21:04:14 -0500

    push more

--- data_read_airliquide.R
+++ data_read_airliquide.R
@@ -41,8 +41,8 @@ library(SparseM)
 library(caTools)
 
 # Use readr to read in the training and test data
-train <- read.csv("./train2.csv")
-test  <-  read.csv("./test.csv")
+train <- read_csv("./train2.csv")
+test  <-  read_csv("./test.csv")
 
 
 #--------- remove html syntax in product description 
@@ -70,40 +70,12 @@ n_words <- function(string){return(sapply(gregexpr("[[:alpha:]]+", string), func
 
 train_pt_words <-  apply(train[3],1,n_words)
 test_pt_words <-  apply(test[3],1,n_words) 
-# train_q_words = apply(train["query"],1,n_words)
-# test_q_words = apply(test["query"],1,n_words)
-# train_pd_words = apply(train[4],1,n_words)
-# test_pd_words = apply(test[4],1,n_words)
 
 
-#------------ compute number of matching words between independent variables 
-# matching_words = function(terms){
-#    term1 = terms[1]
-#    term2 = terms[2]
-#    corpus = Corpus(VectorSource(list(term1, term2))) 
-#    dtm = DocumentTermMatrix(corpus,control=list(tolower=TRUE,removePunctuation=TRUE,removeNumbers=TRUE,stopwords=TRUE,
-#                                                stemming=TRUE))
-#    tf_matrix = as.matrix(dtm)  
-#    matching = sum(tf_matrix[1,]>0 & tf_matrix[2,]>0) 
-#    return(matching)
-# }
-
-#--query & product_title
-# train_q_pt_match = apply(train[,c(2,3)],1,matching_words) 
-# test_q_pt_match = apply(test[,c(2,3)],1,matching_words) 
-
-#--product_description & product_title
-# train_pt_pd_match = apply(train[,c(3,4)],1,matching_words) 
-# test_pt_pd_match = apply(test[,c(3,4)],1,matching_words) 
-# train_pt_pd_pred = ifelse(train_pt_pd_match==0,1,ifelse(train_pt_pd_match>=train_pt_words,4,ifelse(train_pt_pd_match==1,2,3)))
-# test_pt_pd_pred = ifelse(test_pt_pd_match==0,1,ifelse(test_pt_pd_match>=test_pt_words,4,ifelse(test_pt_pd_match==1,2,3)))
 
 #-- combine the sets' queries, p_d(s), p_t(s)
 pt_words <- c(train_pt_words,test_pt_words)
-# q_words = c(train_q_words,test_q_words)
-# pd_words = c(train_pd_words,test_pd_words)
-# q_pt_match = c(train_q_pt_match,test_q_pt_match)
-# pt_pd_pred = c(train_pt_pd_pred,test_pt_pd_pred)
+
 
 
 #------------ bag of words 
@@ -119,17 +91,8 @@ dtm <- DTM(all_text)
 df_q <-  as.data.frame(as.matrix(dtm))
 colnames(df_q) <-  paste("q_",colnames(df_q),sep="")
 
-# all_text = Corpus(VectorSource(combi$product_title))
-# dtm = DTM(all_text)
-# dtm = removeSparseTerms(dtm,0.999)    
-# df_pt = as.data.frame(as.matrix(dtm))
-# colnames(df_pt) = paste("pt_",colnames(df_pt),sep="")
 
-# all_text = Corpus(VectorSource(combi$product_description))
-# dtm = DTM(all_text)
-# dtm = removeSparseTerms(dtm,0.999)   
-# df_pd = as.data.frame(as.matrix(dtm))
-# colnames(df_pd) = paste("pd_",colnames(df_pd),sep="")
+
 
 # Combine all columns into a single dataframe
 combi <-  cbind(df_q,pt_words)  # combi = cbind(df_q,df_pt,df_pd,q_pt_match,pt_words,pt_pd_pred,pd_words,q_words) 
@@ -144,10 +107,8 @@ new_test <-  combi[10159:32671,]
 
 # set.seed(1)
 # spl= sample.split(train_relevance, SplitRatio = 0.7)
-# train_train = subset(new_train, spl==TRUE)
-# train_test = subset(new_train, spl==FALSE)
-# train_train_relevance = subset(train_relevance,spl==TRUE)
-# train_test_relevance = subset(train_relevance,spl==FALSE)
+
+
 
 # sparse_train_train = Matrix(as.matrix(train_train),sparse=T)
 # sparse_train_test = Matrix(as.matrix(train_test),sparse=T)
@@ -162,3 +123,48 @@ model <- svm(sparse_train, train_relevance, kernel="linear", cost=0.3)
 pred <-  predict(model,sparse_test)
 Newsubmission <-  data.frame(id=test_id, prediction = pred)
 write.csv(Newsubmission,"model.csv",row.names=F)      #[1] 0.60718
+
+
+#===============================================================
+# Description: Script that generates a wordcloud out of trigrams extracted from title
+#              corpous for query 'samsonite').
+#              Demonstrates idea to use top trigram in title corpus as surrogate query.
+# Input:       train.csv, test.csv
+# Output:      wordcloud
+
+library(wordcloud)
+library(RWeka)
+library(stringdist)
+library(combinat)
+library(readr)
+library(qdap)
+library(tm)
+
+# Use readr to read in the training and test data
+train <- read_csv("./train2.csv")
+test  <- read_csv("./test.csv")
+dset <- as.data.frame(rbind(train[,1:3], test[1:3]))
+
+# do some cleaing on raw titles
+dset$product_title <- tolower(dset$product_title) # convert to lower case
+dset$product_title <- gsub("[ &<>)(_,.;:!?/-]+", " ", dset$product_title) # replace all punctuation and special characters by space
+dset$product_title <- gsub("'s\\b", "", dset$product_title) # remove the 's
+dset$product_title <- gsub("[']+", "", dset$product_title) # remove '
+dset$product_title <- gsub("[\"]+", "", dset$product_title)   # remove "
+dset$product_title <- gsub(paste("(\\b", paste(stopwords("english"), collapse="\\b|\\b"), "\\b)", sep=""), "", dset$product_title) # remove stopwords
+
+# combine titles into one corpus (grouped by query)
+queries <- unique(dset[,2])
+q <- "samsonite"
+titles <- dset$product_title[dset$query==q]
+trigram <- NGramTokenizer(titles, Weka_control(min = 3, max = 3, delimiters = " \\r\\n.?!:"))
+wd <- as.data.frame(table(trigram))
+wd <- wd[order(-wd$Freq),]
+
+# generate word cloud
+pal2 <- brewer.pal(8,"Dark2")
+# png("alt_query_wordcloud.png", width=12, height=8, units="in", res=300)
+wordcloud(wd$trigram,wd$Freq, scale=c(1.8,0.2),min.freq=1,
+          max.words=200, random.order=FALSE, rot.per=.15, colors=pal2, main="Blog")
+#dev.off()
+
